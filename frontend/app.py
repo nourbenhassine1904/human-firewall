9c37fe88d1a94199afc150dad7fbc1a6a24686a9
Nour BENHASSINE <nourbenhassine340@gmail.com>
2026-04-05 00:43:23 +0100

    Add Docker support and update README with run instructions

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_BASE_URL = "http://127.0.0.1:8000"
+import os
+API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 
 st.set_page_config(page_title="Human Firewall", layout="wide")
 
